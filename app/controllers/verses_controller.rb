4f8cf34b11757999f2e0a0936b70147247ed9ac9
currymich <currymich@gmail.com>
2017-03-15 08:39:17 -0700

    updating comments

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -1,9 +1,11 @@
 class VersesController < ApplicationController
   def search_verse
+    # if multiple verses specified in url, separate into array to be used for multi-verse search
     if params[:verse].include? '-'
       params[:verse] = params[:verse].split('-')
     end
 
+    # use parameters for biblesearch api verse search
     query = {version_id: params[:version], book_id: params[:book], chapter: params[:chapter], verse: params[:verse]}
     result = Bible.verse(query)
 
@@ -11,6 +13,7 @@ class VersesController < ApplicationController
   end
 
   def search_chap
+    # use parameters for biblesearch api chapter search
     query = {version_id: params[:version], book_id: params[:book], chapter: params[:chapter]}
     result = Bible.chap(query)
 
